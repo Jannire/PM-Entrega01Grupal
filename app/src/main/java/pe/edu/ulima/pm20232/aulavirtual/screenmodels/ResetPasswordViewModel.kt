126a5c88fe0c76d817957c60c74e8302b285f8dd
JanoGomezL <132672653+JanoGomezL@users.noreply.github.com>
2023-09-17 16:58:10 -0500

    Add files via upload
    Archivo Screen Model pantalla 2
    Solimano y Gómez

---
+++ app/src/main/java/pe/edu/ulima/pm20232/aulavirtual/screenmodels/ResetPasswordViewModel.kt
@@ -0,0 +1,18 @@
+package pe.edu.ulima.pm20232.aulavirtual.screenmodels
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class ResetPasswordViewModel: ViewModel() {
+    var user: String by mutableStateOf("")
+    var password: String by mutableStateOf("")
+    var bottomSheetCollapse: Boolean by mutableStateOf(true)
+
+    fun access(): Unit{
+        println("BTN PRESSED")
+        println(user)
+        println(password)
+    }
+}
